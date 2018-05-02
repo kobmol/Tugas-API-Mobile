f84c20a1fbdaae841ba65b610ac11428ce9ca94e
kobmol <rendylombok@gmail.com>
2018-05-02 17:09:52 +0700

    'Soalnomor2'

--- App.js
+++ App.js
@@ -1,69 +1,95 @@
-import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right, Footer, FooterTab, Item, Input } from 'native-base';
-import axios from 'axios'
+import React, {Component} from 'react';
+import {Image} from 'react-native';
+import {Container,Header,Item,Input,Text, Button, Icon, Content, Card, CardItem, Thumbnail, Body, Left, Right} from 'native-base';
+import axios from 'axios';
 
-export default class Apply extends Component {
-    constructor(){
-        super()
-        this.state = { 
-            data : [], 
-        }
-    }
+class App extends Component {
 
-    componentWillMount(){
-        axios.get('https://www.thesportsdb.com/api/v1/json/1/lookup_all_teams.php?id=4328')
-        .then (response => {
-            
-            this.setState ({ data : response.data.teams })
-        })
+  constructor(){
+    super()
+    this.state = {isi_data: [], search : ""}
+  }
+
+  get = () => {
+    var url = `https://developers.zomato.com/api/v2.1/search?q=${this.state.search}`;
+
+    var config = {
+      headers:{'user-key':'ecfc3056675b834b4e80bd5385de0624'}
     };
 
-    renderPlayer() {
-        return this.state.data.map((x,i) =>
-        <Card key={i}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={{uri: x.strTeamBadge}} />
-                <Body>
-                  <Text>{x.strTeam}</Text>
-                  <Text note>{x.strLeague}</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem cardBody>
-              <Image source={{uri: x.strTeamFanart1}} style={{height: 300, width: null, flex: 1}}/>
-            </CardItem>
-          </Card>
-        );
-    }
-
-    
-
-  render() {
-    return (
-      <Container>
-        <Header searchBar rounded>
-          <Item>
-            <Icon name="ios-search" />
-            <Input placeholder="Search" />
-            <Icon name="ios-people" />
-          </Item>
-          <Button transparent>
-            <Text>Search</Text>
-          </Button>
-        </Header>
-        <Content>
-          {this.renderPlayer()}
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-            <Text>Player Football</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    );
+    axios.get(url, config)
+    .then((ambilData)=>{
+      this.setState({isi_data: ambilData.data.restaurants})
+      // console.log(this.state.isi_data[0].restaurant.name);
+    })
   }
+
+    render () {
+
+      const data = this.state.isi_data.map((x,index) => {
+
+        var data_nama = x.restaurant.name;
+        var data_kota = x.restaurant.location.city;
+        var data_alamat = x.restaurant.location.address;
+        var data_harga = (x.restaurant.average_cost_for_two/2);
+        var data_gambar = x.restaurant.thumb;
+
+
+        return(
+                <Card key = {index}>
+
+                  <CardItem>
+                    <Left>
+                      <Thumbnail source={{uri: data_gambar}} />
+                      <Body>
+                        <Text>{data_nama}</Text>
+                        <Text note>{data_kota}</Text>
+                      </Body>
+                    </Left>
+
+                    <Right>
+                      <Text>{data_harga}</Text>
+                    </Right>
+                  </CardItem>
+
+                  <CardItem>
+                    <Body>
+                      <Image source = {{uri: data_gambar}} style = {{height: 200, width : 320, flex: 0}} />
+                    </Body>
+                  </CardItem>
+
+                  <CardItem>
+                    <Left>
+                      <Text>{data_alamat}</Text>
+                    </Left>
+                  </CardItem>
+
+                </Card>
+        )
+      })
+
+        return (
+            <Container>
+
+              <Header searchBar rounded>
+                <Item>
+                  <Icon name = "search" />
+                  <Input placeholder = "Masukkan Nama Makanan" onChangeText={(x)=> {this.setState({search: x})}} value={this.state.form}/>
+                </Item>
+              </Header>
+
+              <Header>
+                <Button block onPress={()=> {this.get()}}>
+                  <Text> LIHAT DAFTAR RESTAURANT </Text>
+                </Button>
+              </Header>
+
+              <Content>
+                {data}
+              </Content>
+            </Container>
+        )
+    }
 }
+
+export default App;
